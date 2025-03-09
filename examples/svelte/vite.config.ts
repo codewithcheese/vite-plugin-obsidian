baf949b987af89b7991bee99dddd308294914a28
Tom Manning <mr.tmanning@gmail.com>
2025-03-09 22:39:27 +1000

    Set window.__DEV__ with serverUrl
    Remove esbuild-svelte client should provide

--- examples/svelte/vite.config.ts
+++ examples/svelte/vite.config.ts
@@ -1,9 +1,17 @@
 import obsidian from "@codewithcheese/vite-plugin-obsidian";
 import { defineConfig, UserConfig } from "vite";
+import esbuildSvelte from "esbuild-svelte";
 
 module.exports = defineConfig(async (env): Promise<UserConfig> => {
   const { svelte } = await import("@sveltejs/vite-plugin-svelte");
   return {
-    plugins: [svelte(), obsidian()],
+    plugins: [
+      svelte(),
+      obsidian({
+        extension: {
+          plugins: [esbuildSvelte()],
+        },
+      }),
+    ],
   };
 });
